8531bd4596a12c36b37e7e06e24ef78624533779
Gabriel Gonzaga <124724355+gonzagagabriel@users.noreply.github.com>
2024-07-31 14:03:23 -0300

    Add files via upload

---
+++ Cadastro_pessoas/Cadastro_pessoas/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Cadastro_pessoas.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cadastro_pessoas.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        DbSet<Pessoas> Pessoas { get; set; } = null;
+    }
+    
+}
